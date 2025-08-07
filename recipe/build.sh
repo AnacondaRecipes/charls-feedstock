df953ce54df8cc5fde0e5a204a93706b3030695b
Ivan Iziumov <iiziumov@anaconda.com>
2025-08-07 11:09:16 +0200

    charls 2.4.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,6 @@
 set -ex
-mkdir build
-cd build
 
-cmake ${CMAKE_ARGS} -LAH                                                                \
+cmake -B build ${CMAKE_ARGS} -LAH                                         \
     -DCMAKE_BUILD_TYPE="Release"                                          \
     -DCMAKE_PREFIX_PATH=${PREFIX}                                         \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}                                      \
@@ -11,7 +9,7 @@ cmake ${CMAKE_ARGS} -LAH                                                        
     -DCHARLS_BUILD_TESTS=1                                                \
     -DCHARLS_BUILD_SAMPLES=0                                              \
     -DCHARLS_INSTALL=1                                                    \
-    ..
+    .
 
-make -j${CPU_COUNT}
-make install
+cmake --build build -j --target install --config Release
+ctest -j -C Release --test-dir build
